6bfe5524696dd299e3e8d5fc98eebea7171af8c6
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-02-02 14:15:26 -0800

    recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -28,33 +28,33 @@ def count_words(subreddit, word_list, after="", count=[]):
 
         for topic in (data['data']['children']):
             for word in topic['data']['title'].split():
-                for n in range(len(word_list)):
-                    if word_list[n].lower() == word.lower():
-                        count[n] += 1
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
 
         after = data['data']['after']
         if after is None:
             save = []
-            for n in range(len(word_list)):
-                for m in range(n + 1, len(word_list)):
-                    if word_list[n].lower() == word_list[m].lower():
-                        save.append(m)
-                        count[n] += count[m]
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
 
-            for n in range(len(word_list)):
-                for m in range(n, len(word_list)):
-                    if (count[m] > count[n] or
-                            (word_list[n] > word_list[m] and
-                             count[m] == count[n])):
-                        temp = count[n]
-                        count[n] = count[m]
-                        count[m] = temp
-                        temp = word_list[n]
-                        word_list[n] = word_list[m]
-                        word_list[m] = temp
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
 
-            for n in range(len(word_list)):
-                if (count[n] > 0) and n not in save:
-                    print("{}: {}".format(word_list[n].lower(), count[n]))
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
             count_words(subreddit, word_list, after, count)
